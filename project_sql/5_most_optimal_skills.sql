b0b8c20b40af88eaf958cb01a9dfb141065bc2e3
Lakshay <lakshayarora10@gmail.com>
2024-08-11 20:19:19 +1000

    git commit -m 'message'

---
+++ project_sql/5_most_optimal_skills.sql
@@ -0,0 +1,59 @@
+/*
+Question- What are the most optimal skills based on salary with remote positions only?
+*/
+
+WITH skills_demand AS(
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    count(skills_job_dim.job_id) as demand_count
+FROM 
+    job_postings_fact
+INNER JOIN 
+    skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN 
+    skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Business Analyst'
+    AND
+    salary_year_avg is not NULL
+    AND
+    job_work_from_home = TRUE
+GROUP BY 
+    skills_dim.skill_id
+)
+/*Select *
+FROm skills_demand
+*/
+,
+average_salary AS 
+(
+SELECT
+    skills_job_dim.skill_id,
+   ROUND(AVG(salary_year_avg),0) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN 
+    skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN 
+    skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Business Analyst'
+    AND
+    salary_year_avg is not NULL
+    AND
+    job_work_from_home = TRUE
+GROUP BY 
+    skills_job_dim.skill_id
+)
+
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN average_salary
+    ON  skills_demand.skill_id = average_salary.skill_id
